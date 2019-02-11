e647c802bd618a98da5d62ef6c80497bcc0a3da0
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-11 21:26:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 #----------------------------------------------------
-def move(board, index, value = "X")
+def move(board, index, player = "X")
 board[index]  = value
 return board 
 end
