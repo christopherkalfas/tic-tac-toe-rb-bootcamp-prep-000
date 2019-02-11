ec13ed2e51f533da0a5fe16a9f4a40209030718d
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-11 22:45:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,7 +87,7 @@ def full?(board)
 end
  #----------------------------------------------------
 def draw?(board)
-   if full?(board) && !won?(board)
+   full?(board) && !won?(board) = true
 end 
 
 #----------------------------------------------------
